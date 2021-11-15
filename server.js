41dd2ced050dbe2f9a4f7b6f6e65188de992c122
Dera <stephenie7533@gmail.com>
2021-11-15 08:31:20 +0100

    first commit for heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/', contactRoute);
 
 
-if (process.env.NODE_ENV = "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
     app.get("*", (req, res) =>
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
